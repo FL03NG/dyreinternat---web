b662ed7e5808382cdf88aca1b6947eab7dea3c32
FL03NG <sih013@edu.zealand.dk>
2025-05-14 10:43:04 +0200

    ANimal grid

---
+++ dyreinternat - web/Pages/AnimalsGrid.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dyreinternat___web.Pages
+{
+    public class AnimalsGridModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
