cce106f510164ec9e84d6e26d5a4d5fb3806429e
Mikkel Bauditz Hansen <mih016@edu.zealand.dk>
2025-05-14 10:40:54 +0200

    collection

--- dyreinternat - web/Repository/AnimalCollection.cs
+++ dyreinternat - web/Repository/AnimalCollection.cs
@@ -1,7 +1,46 @@
-﻿namespace dyreinternat___web.Repository
+﻿using dyreinternat___web.Models;
+
+namespace dyreinternat___web.Repository
 {
-    public class AnimalCollection
+    public class AnimalCollection : IAnimal
     {
-        private List<Animal> _animal;
+        private List<Animal> _animals;
+
+        public AnimalCollection()
+        {
+            _animals = new List<Animal>();
+            Seed();
+        }
+
+        public void Add(Animal animal)
+        {
+            _animals.Add(animal);
+        }
+
+        public void Delete(int id)
+        {
+            foreach (Animal a in _animals)
+            {
+                if (a.AnimalID == id)
+                {
+                    _animals.Remove(a);
+                    break;
+                }
+            }
+        }
+
+        public List<Animal> GetAll()
+        {
+            return _animals;
+        }
+
+        private void Seed()
+        {
+            _animals.Add(new Animal(1, "Torben", "Bulldog", "Hund", 4, 8, "Han"));
+            _animals.Add(new Animal(2, "Torbine", "Bulldog", "Hund", 4, 6, "Hun"));
+            _animals.Add(new Animal(3, "Garfield", "Huskat", "Kat", 4, 4, "Han"));
+            _animals.Add(new Animal(4, "Snoop Dogg", "Huskat", "Kat", 4, 5, "Hun"));
+
+        }
     }
 }
