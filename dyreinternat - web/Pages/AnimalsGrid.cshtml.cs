cca209c32d69f002aed55f0445534f167d7f0a3b
FL03NG <sih013@edu.zealand.dk>
2025-05-14 12:45:41 +0200

    filter

--- dyreinternat - web/Pages/AnimalsGrid.cshtml.cs
+++ dyreinternat - web/Pages/AnimalsGrid.cshtml.cs
@@ -7,20 +7,45 @@ using System.Text.Json;
 namespace dyreinternat___web.Pages
 {
     public class AnimalsGridModel : PageModel
-    {
-        public List<Animal> animal = new List<Animal>();
+    {// Den liste, der vises på siden
+        public List<Animal> Animal = new List<Animal>();
+
+        // Søgeord fra brugeren (f.eks. "Garfield")
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
 
         public void OnGet()
         {
-            Seed(); // Fylder listen når siden loades
-        }
+            // 1. Vi laver en liste med alle dyr
+            List<Animal> allAnimals = new List<Animal>();
+            allAnimals.Add(new Animal(1, "Torben", "Bulldog", "Hund", 4, 8, "Han", "bulldog.jpg"));
+            allAnimals.Add(new Animal(2, "Torbine", "Bulldog", "Hund", 4, 6, "Hun", "bulldog2.jpg"));
+            allAnimals.Add(new Animal(3, "Garfield", "Huskat", "Kat", 4, 4, "Han", "cat.jpg"));
+            allAnimals.Add(new Animal(4, "Snoop Dogg", "Huskat", "Kat", 4, 5, "Hun", "orangeCat.jpg"));
 
-        private void Seed()
-        {
-            animal.Add(new Animal(1, "Torben", "Bulldog", "Hund", 4, 8, "Han", "bulldog.jpg"));
-            animal.Add(new Animal(2, "Torbine", "Bulldog", "Hund", 4, 6, "Hun", "bulldog2.jpg"));
-            animal.Add(new Animal(3, "Garfield", "Huskat", "Kat", 4, 4, "Han", "cat.jpg"));
-            animal.Add(new Animal(4, "Snoop Dogg", "Huskat", "Kat", 4, 5, "Hun", "orangeCat.jpg"));
+            // 2. Hvis brugeren har skrevet noget i søgefeltet
+            if (!string.IsNullOrEmpty(SearchTerm))
+            {
+                // 3. Vi går igennem alle dyr én for én
+                for (int i = 0; i < allAnimals.Count; i++)
+                {
+                    Animal a = allAnimals[i];
+
+                    // 4. Hvis navnet indeholder det brugeren har søgt efter (uden store/små bogstaver)
+                    if (a.Name.ToLower().Contains(SearchTerm.ToLower()))
+                    {
+                        Animal.Add(a); // Gem det i den liste, vi vil vise
+                    }
+                }
+            }
+            else
+            {
+                // Hvis der ikke er nogen søgning, vis alle dyr
+                for (int i = 0; i < allAnimals.Count; i++)
+                {
+                    Animal.Add(allAnimals[i]);
+                }
+            }
         }
     }
 }
