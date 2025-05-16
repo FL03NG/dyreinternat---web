84b3146c7dc9c8a5d4898553237501faa87e7a10
Mikkel Bauditz Hansen <mih016@edu.zealand.dk>
2025-05-16 10:11:58 +0200

    create

---
+++ dyreinternat - web/Pages/Book.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dyreinternat___web.Pages
+{
+    public class BookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- dyreinternat - web/Services/AnimalService.cs
+++ dyreinternat - web/Services/AnimalService.cs
@@ -1,6 +1,30 @@
-﻿namespace dyreinternat___web.Services
+﻿using dyreinternat___web.Repository;
+using dyreinternat___web.Models;
+using System.Security.Cryptography.X509Certificates;
+namespace dyreinternat___web.Services
 {
     public class AnimalService
     {
+        private IAnimal _animalrepo;
+
+        public AnimalService(IAnimal animalRepo)
+        {
+            _animalrepo = animalRepo;
+        }
+
+        public void Add(Animal animal)
+        {
+            _animalrepo.Add(animal);
+        }
+
+        public List<Animal> GetAll()
+        {
+            return _animalrepo.GetAll();
+        }
+
+        public void Delete(int id)
+        {
+            _animalrepo.Delete(id);
+        }
     }
 }
