935ce7a11b34c21489d15c8ab28731a956cf961f
FL03NG <sih013@edu.zealand.dk>
2025-05-19 09:17:47 +0200

    hej med dig

--- dyreinternat - web/Repository/AnimalCollection.cs
+++ dyreinternat - web/Repository/AnimalCollection.cs
@@ -5,7 +5,7 @@ namespace dyreinternat___web.Repository
     public class AnimalCollection : IAnimal
     {
         public List<Animal> _animals;
-
+        //hej
         public AnimalCollection()
         {
             _animals = new List<Animal>();
@@ -36,14 +36,7 @@ namespace dyreinternat___web.Repository
 
         private void Seed()
         {
-            _animals.Add(new Animal(1, "Torben", "Bulldog", "Hund", 4, 8, "Han", "bulldog.jpg"));
-            _animals.Add(new Animal(2, "Torbine", "Bulldog", "Hund", 4, 6, "Hun", "bulldog2.jpg"));
-            _animals.Add(new Animal(3, "Garfield", "Huskat", "Kat", 4, 4, "Han", "orangecat.jpg"));
-            _animals.Add(new Animal(4, "Snoop Dogg", "Huskat", "Kat", 4, 5, "Hun", "Cat.jpg"));
-            _animals.Add(new Animal(5, "Egon", "Bulldog", "Hund", 4, 8, "Han", "bulldog.jpg"));
-            _animals.Add(new Animal(6, "Torbine", "Bulldog", "Hund", 4, 6, "Hun", "bulldog2.jpg"));
-            _animals.Add(new Animal(7, "Garfield", "Huskat", "Kat", 4, 4, "Han", "orangecat.jpg"));
-            _animals.Add(new Animal(8, "Snoop Dogg", "Huskat", "Kat", 4, 5, "Hun", "Cat.jpg"));
+        
 
         }
 
